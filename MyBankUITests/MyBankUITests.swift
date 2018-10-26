5cb37b58354d658b4908afeeeb9683b4908c09e8
Johan Martinsson <j.martinsson@bee-buzziness.com>
2018-10-26 15:23:22 +0200

    add id to label errorMessage in storyboard add tests for every validation

--- MyBankUITests/MyBankUITests.swift
+++ MyBankUITests/MyBankUITests.swift
@@ -11,25 +11,45 @@ import XCTest
 class MyBankUITests: XCTestCase {
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
 
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         XCUIApplication().launch()
 
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        
+    
+    
+    func testValidationBalanceMustBeGreaterThanAmount() {
+        let app = XCUIApplication()
+        app.pickerWheels.element.adjust(toPickerWheelValue: "stocks")
+        app.pickerWheels["stocks"].tap()
+        let nextButton = app.buttons["Next"]
+        nextButton.tap()
+        app.pickerWheels["mom"].tap()
+        nextButton.tap()
+        app.textFields["amount"].tap()
+        app.textFields["amount"].typeText("301\n")
+        nextButton.tap()
+        XCTAssertEqual(app.staticTexts["errorMessage"].label, "Amount cannot be greater than 300")
+    }
+    
+    func testValidationBalanceMustBeGreaterThanAmountAndAllowedCredit() {
+        let app = XCUIApplication()
+        app.pickerWheels["current account"].tap()
+        let nextButton = app.buttons["Next"]
+        nextButton.tap()
+        app.pickerWheels["mom"].tap()
+        nextButton.tap()
+        app.textFields["amount"].tap()
+        app.textFields["amount"].typeText("351\n")
+        nextButton.tap()
+        XCTAssertEqual(app.staticTexts["errorMessage"].label, "Amount cannot be greater than 350")
+    }
+
+    func testTransferWorks() {
         let app = XCUIApplication()
         app.pickerWheels["current account"].tap()
         let nextButton = app.buttons["Next"]
@@ -43,4 +63,30 @@ class MyBankUITests: XCTestCase {
         
     }
 
+    func testValidationAmountMustBeNumber() {
+        let app = XCUIApplication()
+        app.pickerWheels["current account"].tap()
+        let nextButton = app.buttons["Next"]
+        nextButton.tap()
+        app.pickerWheels["mom"].tap()
+        nextButton.tap()
+        app.textFields["amount"].tap()
+        app.textFields["amount"].typeText("XXX\n")
+        nextButton.tap()
+        XCTAssertEqual(app.staticTexts["errorMessage"].label, "Amount must be a number")
+    }
+    
+    func testValidationAmountMustBeGreaterThan0() {
+        let app = XCUIApplication()
+        app.pickerWheels["current account"].tap()
+        let nextButton = app.buttons["Next"]
+        nextButton.tap()
+        app.pickerWheels["mom"].tap()
+        nextButton.tap()
+        app.textFields["amount"].tap()
+        app.textFields["amount"].typeText("-1\n")
+        nextButton.tap()
+        XCTAssertEqual(app.staticTexts["errorMessage"].label, "Amount must be greater than zero")
+    }
+    
 }
